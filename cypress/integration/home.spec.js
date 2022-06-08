eb6005c809c69772aa28b94dc2434c4bd8a8fb43
icoolww <williamwijaya.mail@gmail.com>
2022-06-08 03:35:00 +0000

    Adding feature spec: home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+
+   
+describe('Home Page', () => {
+
+  it('Navigates visitor to home page', () => {
+    cy.visit("http://localhost:3000/").should("exist");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+  
+
+});
